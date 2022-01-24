5c403428c8dc081595f7b603dee37af4e9affab6
E.M.C.P. Ekanayake <chamara.sab@outlook.com>
2022-01-24 22:29:45 +0530

    Activity 1- List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './bootstrap.min.css';
+function items(params) {
+    return (
+      <div>{params}</div>
+    );
+}
+function list() {
+  return (
+    <div>
+      <ul class="list-group">
+        <li class="list-group-item list-group-item-action list-group-item-primary">{items("The First item")}</li>
+        <li class="list-group-item list-group-item-action list-group-item-secondary">{items("A second item")}</li>
+        <li class="list-group-item list-group-item-action list-group-item-success">{items("A third item")}</li>
+        <li class="list-group-item list-group-item-action list-group-item-danger">{items("A fourth item")}</li>
+        <li class="list-group-item list-group-item-action list-group-item-warning">{items("And a fifth one")}</li>
+      </ul>
+    </div>
+  );
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='card m-5'>
+        <div className='card-header'>
+          Activity 1
+        </div>
+        <div className='card-body'>
+          <p className='card-title'>Define a component to create an unordered HTML list. List values are passed as parameters.</p>
+          <div className='mt-3'>{list()}</div>
+        </div>
+      </div>
     </div>
   );
 }
